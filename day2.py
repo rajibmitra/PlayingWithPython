3d2995a7bbcc85b9c9654f982a02b0e50ac0213b
Rajib Mitra <rajib.jolite@gmail.com>
2019-01-29 14:16:29 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,14 @@
+"""
+1. Write a program in python that will take numbers from 1 to 50 and but if number is   multiples of three print "PwC" instead of the number and for the multiples of five print "India". And if the number both multiples of three and five it will print “PwC India”. 
+
+2. Traverse a list in reverse order in Python using for loop. 
+
+3. read the file from periodic-table.csv and print out only one row with H details in it. 
+
+4. Write a program in jupyter notebook for if a file exists in a directory , print “File is there!”
+
+5. Write a simple Employee Class in python and method in it which will print the details of a employee. 
+
+6. Write a function that will catch ZeroDivisionError when you try to divide by 0 . 
+
+"""
